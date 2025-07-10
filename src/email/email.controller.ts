0396806c0c719bcaf85e40fad8a8bf68a1037fdb
Keerthivasan-Sparkout <keerthivasan@sparkouttech.com>
2025-07-10 16:24:45 +0530

    Sending email to all user

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller, FileTypeValidator, Get, Param, ParseFilePipe, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { EmailService } from "./email.service";
 import { FileInterceptor } from "@nestjs/platform-express";
 
@@ -9,13 +9,18 @@ export class EmailController {
 
     @Get("/:toemail")
     regiterEmail(@Param('toemail') toemail: string) {
-        this.emailService.attachedHtmlAsPdf( toemail)
+        this.emailService.attachedHtmlAsPdf(toemail, null)
     }
 
     @Post()
     @UseInterceptors(FileInterceptor('newfile'))
-    sendMailUsingExcel(@UploadedFile() file:Express.Multer.File){
-        // console.log(file)
+    sendMailUsingExcel(@UploadedFile(
+        new ParseFilePipe({
+            validators:[
+                new FileTypeValidator({fileType:'sheet'})
+            ]
+        })
+    ) file: Express.Multer.File) {
         this.emailService.getEmailFromExcel(file)
     }
 
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -5,8 +5,7 @@ import * as path from 'path';
 import puppeteer from "puppeteer";
 import * as fs from 'fs';
 import * as handlebars from 'handlebars';
-import * as Excel from 'exceljs';
-import { buffer } from "stream/consumers";
+import * as Excel from 'xlsx';
 
 
 @Injectable()
@@ -19,7 +18,7 @@ export class EmailService {
             to: email,
             from: this.configService.get<string>('formail.user'),
             subject: "Register mail",
-            template: 'salary',
+            template: 'hello',
             context: {
                 name: name ? name : email.split('@')[0]
             },
@@ -32,7 +31,7 @@ export class EmailService {
             to: email,
             from: this.configService.get<string>('formail.user'),
             subject: 'Mail with File',
-            html: `<h1> Hello buddy</h1>`,
+            html: `<h1> Hello </h1>`,
             attachments: [
                 {
                     path: path.join(process.cwd(), 'src', 'images', 'logo.png'),
@@ -43,49 +42,79 @@ export class EmailService {
         });
     }
 
-    async convertHtmlToPdf(): Promise<Buffer> {
+    async convertHtmlToPdf(user: any): Promise<Buffer> {
         const browser = await puppeteer.launch()
         const tab = await browser.newPage()
         const file = fs.readFileSync(path.join(process.cwd(), 'src', 'email', 'temp', 'salary.hbs'), 'utf-8');
-        // const sourcefile = handlebars.compile(file)
-
-        tab.setContent(file)
+        const sourcefile = handlebars.compile(file)
+        tab.setContent(sourcefile(this.generateSalaryObject(user)))
         const buffer = await tab.pdf({ format: 'A4', printBackground: true }) as Buffer;
-
         return buffer;
     }
 
+    generateSalaryObject(user: any) {
+        return ({
+            empId: user["empId"],
+            name: user["name "],
+            designation: user["Designation"],
+            dateOfJoining: user["Date of joining"],
+            monthYear: user["month/year"],
+            noOfDays: user["no of days"],
+            basicPay: user["Basic Pay"],
+            Hra: user["HRA"],
+            conveyance: user["Conveyance"],
+            foodAllowance: user["Food Allowance"],
+            eductaionAllowance: user["Educational Allowance"],
+            specialAllowance: user["Special Allowance"],
+            performanceBonus: user["Performance Bonus"],
+            overtimePay: user["Overtime Pay"],
+            totalEarning: user["Total earning"],
+            epf: user["EPF"],
+            esi: user["ESI"],
+            tds: user["TDS"],
+            professionalTax: user["Proffessional Tax"],
+            lossOfPay: user["Loss of Pay"],
+            salaryAdvance: user["salary in Advance"],
+            otherDeduction: user["other deduction"],
+            healthInsurance: user["Health Insurance"],
+            totalDeduction: user["Total Deduction"],
+            rupees: user["Rupees"],
+            inword: user["In word"]
 
-    async attachedHtmlAsPdf(toemail: string) {
-        this.mailerService.sendMail({
+        })
+    }
+
+
+    async attachedHtmlAsPdf(toemail: string, user: any) {
+        await this.mailerService.sendMail({
             to: toemail,
             from: this.configService.get<string>('formail.user'),
             subject: "salary mail",
             html: "<p> your salary has been crited </p>",
             attachments: [{
-                content: await this.convertHtmlToPdf(),
+                content: await this.convertHtmlToPdf(user),
                 filename: 'salary slip.pdf',
                 contentType: 'application/pdf'
             }]
         })
     }
 
-    // async getEmailFromExcel(file: Express.Multer.File) {
-    //     const workbook =await  Excel.read(file.buffer, { type: 'buffer' })
-    //     const sheetName = workbook.SheetNames[0]
-    //     const workSheet = workbook.Sheets[sheetName]
-    //     const rows=await Excel.utils.sheet_to_json(workSheet);
-    //     rows.forEach((row)=>{
-    //         console.log(row)
-    //     })
-    // }
 
     async getEmailFromExcel(file: Express.Multer.File) {
-        console.log("----------------")
-        const workbook = new Excel.Workbook()
-        const workSheet = await workbook.xlsx.load(file.buffer)
-        const sheet=workSheet.getWorksheet(1);
-        sheet?.eachRow(data =>{
-        })
+        if (file) {
+            const workbook = await Excel.read(file.buffer, { type: 'buffer' });
+            const sheetName = workbook.SheetNames[1];
+            const workSheet = workbook.Sheets[sheetName];
+            const rows = Excel.utils.sheet_to_json(workSheet, { raw: false });
+            if(rows.length===0){
+                throw new Error("this sheet does't contain rows")
+            }
+            rows.forEach(async row => { row? await this.attachedHtmlAsPdf(row['email'],row):null
+            })
+        } else {
+            throw new handlebars.Exception("File is missing")
+        }
+
     }
+
 }
